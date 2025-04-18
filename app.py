b15e18cc33d8490c1839c7bc2cb258ebe0675607
Koji Ino <kojiino@outlook.com>
2025-04-17 20:46:46 -1000

    added leaderboard posting function. not implimented to be used yet

--- app.py
+++ app.py
@@ -17,6 +17,10 @@ last_to_post_channel_id = os.getenv('CHANNEL_ID')
 db_path = os.getenv('DATABASE_PATH')
 bot_id = os.getenv('BOT_ID')
 
+
+# Channel to use
+posting_channel_id = test_channel
+
 app = Flask(__name__)
 slack_client = WebClient(token=bot_token)
 slack_event_adapter = SlackEventAdapter(signing_secret, '/slack/events', app)
@@ -39,10 +43,10 @@ def handle_message(event_data):
     global last_time
     print(f"Received message: {text} from channel: {channel_id} by user: {user_id}")
 
-    if channel_id == test_channel and user_id != bot_id:
+    if channel_id == posting_channel_id and user_id != bot_id:
         if user_id != last_person_id:
             slack_client.chat_postMessage(
-                channel=test_channel,
+                channel=posting_channel_id,
                 text=f"Last person to post: <@{user_id}> \nThe previous person, (<@{last_person_id}>) was the last to post for {time.time() - last_time} seconds."
             )
 
@@ -67,5 +71,37 @@ def handle_message(event_data):
 
 
 
+def send_leaderboard():
+    try:
+        db = sqlite3.connect(db_path)
+        cursor = db.cursor()
+    except Exception as e:
+        print(f"Error connecting to the database: {e}")
+        exit(1)
+
+    cursor.execute("SELECT * FROM time_as_last WHERE date = ? ORDER BY time DESC;", (f"{datetime.date.today().year}-{datetime.date.today().month}-{datetime.date.today().day}",))
+    rows = cursor.fetchall()
+    db.close()
+    
+    
+    yesterday = datetime.date.today() - datetime.timedelta(days=1)
+    month = yesterday.month
+    day = yesterday.day
+    leaderboard = f"The daily leaderboard has reset! Here is the rankings for the most total time with the last post for {month}/{day}:\n"
+    
+    for row in rows:
+        user_id = row[1]
+        time = row[2]
+        leaderboard += f"`<@{user_id}>`: {round(time)} seconds\n"
+    
+    leaderboard += "\n\nAs of now everyone's time has been set back to 0 seconds.\n\n"
+
+    slack_client.chat_postMessage(
+        channel=posting_channel_id,
+        text=leaderboard
+    )
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='127.0.0.1', port=2002)
