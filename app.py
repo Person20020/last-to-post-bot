d72044ac1eec310b5748aefba0833f7781cee63e
Koji Ino <kojiino@outlook.com>
2025-04-20 09:00:41 -1000

    updated leaedrboard

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ import time
 import threading
 
 
+# Add time zones using pytz
+
+
 """
 .env file variables
 SLACK_BOT_TOKEN=
@@ -32,8 +35,15 @@ db_path = os.getenv('DATABASE_PATH')
 bot_id = os.getenv('BOT_ID')
 
 
+# Set the mode for testing
+test_mode = True
+
 # Channel to use
-posting_channel_id = test_channel
+if test_mode:
+    posting_channel_id = test_channel
+else:
+    posting_channel_id = last_to_post_channel_id
+
 
 app = Flask(__name__)
 slack_client = WebClient(token=bot_token)
@@ -55,39 +65,76 @@ def handle_message(event_data):
     text = message['text']
     global last_person_id
     global last_time
-    print(f"\n\033[31mReceived message: {text} from channel: {channel_id} by user: {user_id}\033[0m")
+    if test_mode:
+        print(f"\n\033[31mReceived message: {text} from channel: {channel_id} by user: {user_id}\033[0m")
 
     if channel_id == posting_channel_id and user_id != bot_id:
         if slack_client.users_info(user=user_id)['user']['is_bot']:
             print("Ignoring bot message.")
             return
-        if user_id != last_person_id:
-            slack_client.chat_postMessage(
-                channel=posting_channel_id,
-                text=f"Last person to post: <@{user_id}> \nThe previous person, (<@{last_person_id}>) was the last to post for {time.time() - last_time} seconds."
-            )
 
-            if last_person_id is not None:
-                try:
-                    db = sqlite3.connect(db_path)
-                    cursor = db.cursor()
-                except Exception as e:
-                    print(f"Error connecting to the database: {e}")
-                    exit(1)
+        if last_person_id is not None:
+            if test_mode: # If in testing mode send debug message
+                slack_client.chat_postMessage(
+                    channel=posting_channel_id,
+                    text=f"Last person to post: <@{user_id}> \nThe previous person, (<@{last_person_id}>) was the last to post for {round((time.time() - last_time), 2)} seconds."
+                )
+            
+            try:
+                db = sqlite3.connect(db_path)
+                cursor = db.cursor()
+            except Exception as e:
+                print(f"Error connecting to the database: {e}")
+                exit(1)
 
-                if not cursor.execute("SELECT * FROM time_as_last WHERE user_id = ? AND date = ?;", (last_person_id, f"{datetime.date.today().year}-{datetime.date.today().month}-{datetime.date.today().day}")).fetchone():
-                    cursor.execute("INSERT INTO time_as_last (user_id, date, time) VALUES (?, ?, ?);", (last_person_id, f"{datetime.date.today().year}-{datetime.date.today().month}-{datetime.date.today().day}", time.time() - last_time))
-                else:
-                    cursor.execute("UPDATE time_as_last SET time = time + ? WHERE user_id = ? AND date = ?;", (time.time() - last_time, last_person_id, f"{datetime.date.today().year}-{datetime.date.today().month}-{datetime.date.today().day}"))
-                db.commit()
-                db.close()
-            last_person_id = user_id
-            last_time = time.time()
+            if not cursor.execute("SELECT * FROM time_as_last WHERE user_id = ? AND date = ?;", (last_person_id, f"{datetime.date.today().year}-{datetime.date.today().month}-{datetime.date.today().day}")).fetchone():
+                cursor.execute("INSERT INTO time_as_last (user_id, date, time) VALUES (?, ?, ?);", (last_person_id, f"{datetime.date.today().year}-{datetime.date.today().month}-{datetime.date.today().day}", time.time() - last_time))
+            else:
+                cursor.execute("UPDATE time_as_last SET time = time + ? WHERE user_id = ? AND date = ?;", (time.time() - last_time, last_person_id, f"{datetime.date.today().year}-{datetime.date.today().month}-{datetime.date.today().day}"))
+            db.commit()
+            db.close()
+        last_person_id = user_id
+        last_time = time.time()
     return 
 
 
 
 def send_leaderboard():
+    print("Logging current person's time...")
+    global last_person_id
+    global last_time
+
+    print("Last person ID: ", last_person_id)
+    print("Duration since last post: ", round((time.time() - last_time), 2), "\n")
+
+    if last_person_id is not None:
+        print("Last person ID: ", last_person_id)
+        print("Duration since last post: ", round((time.time() - last_time), 2))
+        if test_mode: # If in testing mode send debug message
+            slack_client.chat_postMessage(
+                channel=posting_channel_id,
+                text=f"Last person to post: <@{last_person_id}> \nLogging {round((time.time() - last_time), 2)} seconds of time."
+            )
+        
+        try:
+            db = sqlite3.connect(db_path)
+            cursor = db.cursor()
+        except Exception as e:
+            print(f"Error connecting to the database: {e}")
+            exit(1)
+
+        if not cursor.execute("SELECT * FROM time_as_last WHERE user_id = ? AND date = ?;", (last_person_id, f"{datetime.date.today().year}-{datetime.date.today().month}-{datetime.date.today().day}")).fetchone():
+            cursor.execute("INSERT INTO time_as_last (user_id, date, time) VALUES (?, ?, ?);", (last_person_id, f"{datetime.date.today().year}-{datetime.date.today().month}-{datetime.date.today().day}", time.time() - last_time))
+        else:
+            cursor.execute("UPDATE time_as_last SET time = time + ? WHERE user_id = ? AND date = ?;", (time.time() - last_time, last_person_id, f"{datetime.date.today().year}-{datetime.date.today().month}-{datetime.date.today().day}"))
+        db.commit()
+        db.close()
+        last_time = time.time()
+
+    print("Logging complete.")
+
+
+
     print("Retrieving leaderboard data...")
 
     try:
@@ -97,10 +144,17 @@ def send_leaderboard():
         print(f"Error connecting to the database: {e}")
         exit(1)
     
-    yesterday = datetime.date.today() - datetime.timedelta(days=1)
-    year = yesterday.year
-    month = yesterday.month
-    day = yesterday.day
+    # If in test mode use today's date so i can test the leaderboard with live changing data
+    if test_mode:
+        today = datetime.date.today()
+        year = today.year
+        month = today.month
+        day = today.day
+    else:
+        yesterday = datetime.date.today() - datetime.timedelta(days=1)
+        year = yesterday.year
+        month = yesterday.month
+        day = yesterday.day
 
     cursor.execute("SELECT * FROM time_as_last WHERE date = ? ORDER BY time DESC;", (f"{year}-{month}-{day}",))
     rows = cursor.fetchall()
@@ -118,29 +172,32 @@ def send_leaderboard():
 
     leaderboard = f"The daily leaderboard has reset! Here is the rankings for the most total time with the last post for {month}-{day}-{year}:\n"
     
-    for row in rows:
+    for row, i in zip(rows, range(len(rows))):
         user_id = row[1]
         try:
             username = slack_client.users_info(user=user_id)['user']['profile']['display_name']
         except Exception as e:
             print(f"Error fetching user info: {e}")
             username = "Unknown User"
-        time = row[2]
-        round_time = round(time)
+        user_time = row[2]
+        round_time = round(user_time)
         seconds = round_time % 60
         total_minutes = round_time // 60
         minutes = total_minutes % 60
         hours = round_time // 3600
-        if time < 1 and time != 0:
-            leaderboard += f"`@{username}`:    Less than 1 second\n"
+        if user_time < 1 and user_time != 0:
+            leaderboard += f"{i+1}: `@{username}`:    Less than 1 second\n"
+        else:
+            converted_time = f"{hours}:{minutes:02}:{seconds:02}"
+            leaderboard += f"{i+1}: `@{username}`:    {converted_time}\n"
+        
+        """
         elif round_time < 60:
             leaderboard += f"`@{username}`:    {round_time} seconds\n"
         elif round_time < 3600:
             converted_time = f"0:{minutes:02}:{seconds:02}"
             leaderboard += f"`@{username}`:    {converted_time}\n"
-        else:
-            converted_time = f"{hours}:{minutes:02}:{seconds:02}"
-            leaderboard += f"`@{username}`:    {converted_time}\n"
+        """
     
     leaderboard += "\n\nAs of now, it is a new day and everyone's time is starting from 0 seconds.\n\n"
 
@@ -168,9 +225,9 @@ def run_schedules():
 
 
 if __name__ == '__main__':
+    print("Starting app...")
     if not os.environ.get("WERKZEUG_RUN_MAIN"):
         schedule.every().minute.at(":00").do(send_leaderboard)
         schedule_thread = threading.Thread(target=run_schedules, daemon=True)
         schedule_thread.start()
     app.run(debug=True, host='127.0.0.1', port=2002)
-    
---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sleep 3
+source /home/$USER/last-to-post-bot-venv/bin/activate
+python /home/$USER/last-to-post-bot-venv/last-to-post-bot/app.py
