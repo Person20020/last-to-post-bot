53fbf7ef052bbeb0d7ebaf3a1c07430761028efc
Koji Ino <kojiino@outlook.com>
2025-04-20 14:51:31 -1000

    Added changeable time zone support in dotenv and moved the testing variables to dotenv.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import datetime
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, url_for
 import os
+import pytz
 import schedule
 from slack_sdk import WebClient
 from slackeventsapi import SlackEventAdapter
@@ -10,22 +11,18 @@ import time
 import threading
 
 
-# Set the mode for testing
-test_mode = False
-test_channel_mode = True
-test_date_mode = True
-
-# Add time zones using pytz
-
-
 """
 .env file variables
+
 SLACK_BOT_TOKEN=
 SLACK_SIGNING_SECRET=
 TEST_CHANNEL_ID=
 CHANNEL_ID=
 DATABASE_PATH=
 BOT_ID=
+TEST_MODE=
+TEST_CHANNEL_MODE=
+TEST_DATE_MODE=
 """
 
 
@@ -39,7 +36,35 @@ last_to_post_channel_id = os.getenv('CHANNEL_ID')
 db_path = os.getenv('DATABASE_PATH')
 bot_id = os.getenv('BOT_ID')
 
+test_mode = os.getenv('TEST_MODE')
+test_channel_mode = os.getenv('TEST_CHANNEL_MODE')
+test_date_mode = os.getenv('TEST_DATE_MODE')
+timezone_location = os.getenv('TIMEZONE')
 
+
+if test_mode == "True":
+    test_mode = True
+else:
+    test_mode = False
+if test_channel_mode == "True":
+    test_channel_mode = True
+else:
+    test_channel_mode = False
+if test_date_mode == "True":
+    test_date_mode = True
+else:
+    test_date_mode = False
+if timezone_location == "":
+    timezone_location = 'GMT'
+    print("No timezone set, defaulting to GMT.")
+else:
+    print("Timezone set to: ", timezone_location)
+    if timezone_location not in pytz.all_timezones:
+        print("Invalid timezone, defaulting to GMT.")
+        timezone_location = 'GMT'
+
+
+timezone = pytz.timezone(timezone_location)
 
 # Channel to use
 if test_channel_mode:
@@ -69,6 +94,9 @@ last_time = time.time()
 @app.route('/')
 def home():
     return "Hello, World!"
+
+
+
 
 @slack_event_adapter.on('message')
 def handle_message(event_data):
@@ -134,7 +162,8 @@ def handle_message(event_data):
 
 
 
-def send_leaderboard():
+
+def log_time():
     print("Logging current person's time...")
     
     try:
@@ -166,13 +195,13 @@ def send_leaderboard():
             db = sqlite3.connect(db_path)
             cursor = db.cursor()
             if test_date_mode:
-                today = datetime.date.today()
+                today = datetime.datetime.now(timezone).date()
                 if not cursor.execute("SELECT * FROM time_as_last WHERE user_id = ? AND date = ?;", (last_person_id, f"{today.year}-{today.month}-{today.day}")).fetchone():
                     cursor.execute("INSERT INTO time_as_last (user_id, date, time) VALUES (?, ?, ?);", (last_person_id, f"{today.year}-{today.month}-{today.day}", time.time() - last_time))
                 else:
                     cursor.execute("UPDATE time_as_last SET time = time + ? WHERE user_id = ? AND date = ?;", (time.time() - last_time, last_person_id, f"{today.year}-{today.month}-{today.day}"))
             else:
-                yesterday = datetime.date.today() - datetime.timedelta(days=1)
+                yesterday = datetime.datetime.now(timezone).date() - datetime.timedelta(days=1)
                 if not cursor.execute("SELECT * FROM time_as_last WHERE user_id = ? AND date = ?;", (last_person_id, f"{(yesterday.year)}-{yesterday.month}-{yesterday.day}")).fetchone():
                     cursor.execute("INSERT INTO time_as_last (user_id, date, time) VALUES (?, ?, ?);", (last_person_id, f"{yesterday.year}-{yesterday.month}-{yesterday.day}", time.time() - last_time))
                 else:
@@ -199,17 +228,85 @@ def send_leaderboard():
 
 
 
+
+def send_leaderboard():
+    """
+    print("Logging current person's time...")
+    
+    try:
+        db = sqlite3.connect(db_path)
+        cursor = db.cursor()
+        cursor.execute('SELECT last_user_id, last_time FROM shared_data WHERE id = 1;')
+        shared_data = cursor.fetchone()
+        db.close()
+        last_person_id = shared_data[0]
+        last_time = shared_data[1]
+    except Exception as e:
+        print(f"Error connecting to the database: {e}")
+        exit(1)
+
+
+    print("Last person ID: ", last_person_id)
+    print("Duration since last post: ", round((time.time() - last_time), 2), "\n")
+
+    if last_person_id is not None:
+        print("Last person ID: ", last_person_id)
+        print("Duration since last post: ", round((time.time() - last_time), 2))
+        if test_mode: # If in testing mode send debug message
+            slack_client.chat_postMessage(
+                channel=posting_channel_id,
+                text=f"Last person to post: <@{last_person_id}> \nLogging {round((time.time() - last_time), 2)} seconds of time."
+            )
+        
+        try:
+            db = sqlite3.connect(db_path)
+            cursor = db.cursor()
+            if test_date_mode:
+                today = datetime.datetime.now(timezone).date()
+                if not cursor.execute("SELECT * FROM time_as_last WHERE user_id = ? AND date = ?;", (last_person_id, f"{today.year}-{today.month}-{today.day}")).fetchone():
+                    cursor.execute("INSERT INTO time_as_last (user_id, date, time) VALUES (?, ?, ?);", (last_person_id, f"{today.year}-{today.month}-{today.day}", time.time() - last_time))
+                else:
+                    cursor.execute("UPDATE time_as_last SET time = time + ? WHERE user_id = ? AND date = ?;", (time.time() - last_time, last_person_id, f"{today.year}-{today.month}-{today.day}"))
+            else:
+                yesterday = datetime.datetime.now(timezone).date() - datetime.timedelta(days=1)
+                if not cursor.execute("SELECT * FROM time_as_last WHERE user_id = ? AND date = ?;", (last_person_id, f"{(yesterday.year)}-{yesterday.month}-{yesterday.day}")).fetchone():
+                    cursor.execute("INSERT INTO time_as_last (user_id, date, time) VALUES (?, ?, ?);", (last_person_id, f"{yesterday.year}-{yesterday.month}-{yesterday.day}", time.time() - last_time))
+                else:
+                    cursor.execute("UPDATE time_as_last SET time = time + ? WHERE user_id = ? AND date = ?;", (time.time() - last_time, last_person_id, f"{yesterday.year}-{yesterday.month}-{yesterday.day}"))
+            db.commit()
+            db.close()
+        except Exception as e:
+            print(f"Error connecting to the database: {e}")
+            exit(1)
+
+        
+        last_time = time.time()
+        try:
+            db = sqlite3.connect(db_path)
+            cursor = db.cursor()
+            cursor.execute('UPDATE shared_data SET last_time = ? WHERE id = 1;', (last_time,))
+            db.commit()
+            db.close()
+        except Exception as e:
+            print(f"Error connecting to the database: {e}")
+            exit(1)
+
+    print("Logging complete.")
+    """
+
+    log_time()
+
     print("Retrieving leaderboard data...")
 
     
     # If in test mode use today's date so i can test the leaderboard with live changing data
     if test_date_mode:
-        today = datetime.date.today()
+        today = datetime.datetime.now(timezone).date()
         year = today.year
         month = today.month
         day = today.day
     else:
-        yesterday = datetime.date.today() - datetime.timedelta(days=1)
+        yesterday = datetime.datetime.now(timezone).date() - datetime.timedelta(days=1)
         year = yesterday.year
         month = yesterday.month
         day = yesterday.day
@@ -268,6 +365,30 @@ def send_leaderboard():
 
 
 
+
+def schedule_checker():
+    while True:
+        try:
+            now = datetime.datetime.now(timezone)
+            if test_date_mode:
+                if now.second == 0:
+                    send_leaderboard()
+                    time.sleep(1.1)
+            else:
+                if now.hour == 0 and now.minute == 0 and now.second == 0:
+                    send_leaderboard()
+                    time.sleep(1.1)
+        except KeyboardInterrupt as e:
+            print("Exiting...")
+            log_time()
+            exit(0)
+        except Exception as e:
+            print(f"Error in scheduler function: {e}")
+            time.sleep(1)
+
+
+
+
 def run_schedules():
     while True:
         try:
@@ -282,15 +403,14 @@ def run_schedules():
 
 
 
+
 if __name__ == '__main__':
     print("Starting app...")
     if not os.environ.get("WERKZEUG_RUN_MAIN"):
-        if test_date_mode:
-            schedule.every().minute.at(":00").do(send_leaderboard)
-        else:
-            schedule.every().day.at("00:00").do(send_leaderboard)
-        schedule_thread = threading.Thread(target=run_schedules, daemon=True)
-        schedule_thread.start()
+        print("Starting schedule checker thread...")
+        schedule_function_thread = threading.Thread(target=schedule_checker, daemon=True)
+        schedule_function_thread.start()
+        print("Scheduler thread started.")
     if test_mode:
         app.run(debug=True, host='127.0.0.1', port=2002)
     else:
